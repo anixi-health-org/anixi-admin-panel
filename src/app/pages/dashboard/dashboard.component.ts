e666a0f69b649c1384116ffef19650435c221fe8
klist1337 <meneur2017@gmail.com>
2026-03-19 10:57:55 +0000

    add recent activity and quickActions button to dashboard

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { EChartsCoreOption } from 'echarts/core';
+import { color, EChartsCoreOption } from 'echarts/core';
 
 
 const StaticCards1 = [
@@ -7,12 +7,34 @@ const StaticCards1 = [
   {label: 'Active Users', icon: 'activity', value: '244', index: '1'},
   {label: 'Communities', icon: 'message-square', value: '42', index: '2'},
   {label: 'Articles', icon: 'file-text', value: '45', index: '3'},
-]
+];
 const staticCards2 = [
   {label: 'Notifications Sent', icon: 'bell', value: '26', index: '4'},
   {label: 'Report Pending', icon: 'triangle-alert', value: '23', index: '5'},
   {label: 'Doctors Pending', icon: 'stethoscope', value: '0', index: '6'},
   {label: 'Suspended Users', icon: 'user-x', value: '0', index: '7'},
+];
+
+const activities = [
+  { index: '0',type: 'doctor', title: 'New doctor application', content: 'Dr. Sarah Molefe submitted verification', 
+    bgColor: '#ebf5f3' , color: '#27a288' , createdAt: '5 min ago'},
+  { index: '1',type: 'content', title: 'Content published', content: '"Managing Diabete in Summer" went live', 
+    bgColor: '#ebf5fb' , color: '#6fb9e4', createdAt: '22 min ago'},
+  { index: '2',type: 'report', title: 'User reported', content: 'Post flagged in HIV/AIDS community', 
+    bgColor: '#fcebea' , color: '#dc2928', createdAt: '1 hr ago'},
+  { index: '3',type: 'notification', title: 'Campaign sent', content: 'Weekly health tips to 8432 users', 
+    bgColor: '#fef6e9' , color: '#492d03', createdAt: '2 hrs ago'},
+  { index: '4',type: 'doctor', title: 'Doctor approved', content: 'Dr. Thabo Nkosi verified and badged', 
+    bgColor: '#ebf5f3' , color: '#27a288', createdAt: '3 hrs ago'},
+  { index: '5',type: 'moderation', title: 'User suspended', content: 'Account @toxic_user22 suspended for violations', 
+    bgColor: '#fef6e9' , color: '#f69e23', createdAt: '5 hrs ago'},
+];
+const quickActions = [
+  {index: '0', action: 'Create Article',  icon: 'file-text'},
+  {index: '1', action: 'Send Notification',  icon: 'bell'},
+  {index: '2', action: 'Review Doctor',  icon: 'stethoscope'},
+  {index: '3', action: 'View Reports',  icon: 'triangle-alert'},
+  {index: '3', action: 'Manage Users',  icon: 'users'},
 ]
 
 @Component({
@@ -25,6 +47,8 @@ const staticCards2 = [
 export class DashboardComponent {
   cards = StaticCards1;
   cards2 = staticCards2;
+  activities = activities;
+  actions = quickActions;
   options: EChartsCoreOption = {
     color: ['#21a086'],
     tooltip : {
