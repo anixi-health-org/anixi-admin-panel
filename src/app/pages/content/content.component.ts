96b65979446494f91ff7c7bae2eab89d799f9222
klist1337 <meneur2017@gmail.com>
2026-03-23 19:42:40 +0100

    articles table is almost done

--- src/app/pages/content/content.component.ts
+++ src/app/pages/content/content.component.ts
@@ -6,7 +6,10 @@ const contentCards = [
   {index: '1' , label: 'Published', value: '245' },
   {index: '2' , label: 'Scheduled', value: '28' },
   {index: '3' , label: 'Drafts', value: '39' },
-]
+];
+
+
+
 
 @Component({
   selector: 'app-content',
@@ -18,4 +21,30 @@ const contentCards = [
 export class ContentComponent {
   cards = contentCards;
   search = new FormControl('');
+
+
+  getColorByStatus(status: string) {
+    switch(status) {
+      case 'Published':
+        return {
+          bgColor: '#ebf6f0',
+          color: '#2cab6f'
+        }
+      case 'Schedule':
+        return {
+          bgColor:  '#2cab6f',
+          color: '#269ed9',
+        }
+      case 'Draft':
+        return {
+          bgColor: '#f9fafb',
+          color: '#66758a'
+        }
+      default:
+        return {
+          bgColor: '#f1f2f4',
+          color: '#9da7b4'
+        }
+    }
+  }
 }
